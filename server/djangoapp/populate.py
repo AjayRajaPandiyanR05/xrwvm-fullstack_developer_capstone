65c6ac8bcba042a52e68039f0ced6f450c54d06b
AjayRajaPandiyanR05 <ajayrajapandiyanr@email.com>
2024-05-09 10:41:45 -0400

    ESLINT

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -109,7 +109,7 @@ def initiate():
             "car_make": car_make_instances[4]
         }
     ]
-    
+
     for data in car_model_data:
         CarModel.objects.create(
             name=data['name'],
