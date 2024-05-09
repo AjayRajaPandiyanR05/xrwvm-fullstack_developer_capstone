15a70cb3fd0bbe07d6ca3583b705adea134d31e3
AjayRajaPandiyanR05 <ajayrajapandiyanr@email.com>
2024-05-09 10:17:13 -0400

    ESLINT

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -16,13 +16,13 @@ class CarMake(models.Model):
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    CAR_TYPES = [('SEDAN', 'Sedan'),('SUV', 'SUV'),('WAGON', 'Wagon')]
+    CAR_TYPES = [('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon')]
     type = models.CharField(
-        max_length=100, 
-        choices=CAR_TYPES, 
+        max_length=100,
+        choices=CAR_TYPES,
         default='SUV')
-    year = models.IntegerField(default = 2024, validators = [
+    year = models.IntegerField(default=2024, validators=[
         MaxValueValidator(2024), MinValueValidator(2015)])
-    
+
     def __str__(self):
         return self.name
